86428aa64492495d70ad480ef3cbb87935a4ec9f
brandypena <brandy.pena@gmail.com>
2016-11-22 14:58:42 -0600

    completed assignment locally

---
+++ Product_Assignment_blp876.java
@@ -0,0 +1,11 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_Assignment_blp876.java   (Product_jr007.java)
+// It should have a private sku instance variable containing your eid.
+
+
+class Product_Assignment_blp876 {
+	private string eid = "blp876";
+}
+
+
